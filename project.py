e6a5140a9405927dfe29e1a2fab8afb23545bd1e
JAYBUDDHA <ajaykumarsam1729@gamil.com>
2024-02-22 17:49:26 +0530

    calculator

--- project.py
+++ project.py
@@ -1,2 +1,61 @@
-for i in range(10):
-    print((i+1) * 2)
+import math
+
+def main():
+    print("Select operation:")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+    print("5. Power")
+    print("6. Square Root")
+
+    while True:
+        choice = input("Enter choice (1/2/3/4/5/6): ")
+
+        if choice in ('1', '2', '3', '4', '5'):
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+
+            if choice == '1':
+                print("Result:", add(num1, num2))
+            elif choice == '2':
+                print("Result:", subtract(num1, num2))
+            elif choice == '3':
+                print("Result:", multiply(num1, num2))
+            elif choice == '4':
+                print("Result:", divide(num1, num2))
+            elif choice == '5':
+                print("Result:", power(num1, num2))
+        elif choice == '6':
+            num = float(input("Enter a number: "))
+            print("Result:", sqrt(num))
+        else:
+            print("Invalid input")
+
+        next_calculation = input("Do you want to perform another calculation? (yes/no): ")
+        if next_calculation.lower() != "yes":
+            break
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        return "Error! Division by zero."
+    else:
+        return x / y
+
+def power(x, y):
+    return x ** y
+
+def sqrt(x):
+    return math.sqrt(x)
+
+if __name__=="__main__":
+    main()
